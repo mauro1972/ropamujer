56a45f3443d91e0d6eff463920dd92ff825086f7
yacu72 <yacu72@gmail.com>
2015-09-05 11:08:50 -0300

    flex slide style

--- js/omega_kickstart.js
+++ js/omega_kickstart.js
@@ -56,14 +56,4 @@
 
 })(jQuery);
 
-    // Can also be used with $(document).ready()
-  $(window).load(function() {
-    $('.flexslider').flexslider({
-      animation: "slide",
-      animationLoop: false,
-      itemWidth: 210,
-      itemMargin: 5,
-      minItems: 2,
-      maxItems: 4
-    });
-  });
+
